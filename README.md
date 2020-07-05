8bccac235f3195b3fdf51b09e3cc0cd8bfbe965f
Semprini Mattia <mattia.semprini2@studio.unibo.it>
2020-07-05 12:07:29 +0200

    Start basic develop using official material design example

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AppartamentoFacile
 Coltellino svizzero per un proprietario di casa affaccendato.
+
+## Acknowledgments
+* Guide used by example: <a href="https://codelabs.developers.google.com/codelabs/mdc-101-java/" title="MDC">MDC 101</a>
+* Home icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
--- app/src/main/java/com/example/appartamentofacile/LoginFragment.java
+++ app/src/main/java/com/example/appartamentofacile/LoginFragment.java
@@ -1,4 +1,25 @@
 package com.example.appartamentofacile;
 
-class LoginFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.af_login_fragment, container, false);
+
+        // Snippet from "Navigate to the next Fragment" section goes here.
+
+        return view;
+    }
+
+    // "isPasswordValid" from "Navigate to the next Fragment" section method goes here
 }
